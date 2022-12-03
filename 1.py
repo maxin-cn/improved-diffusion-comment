bc4f5896ac7f176bbc54094c7658bc24d48b4129
maxin-cn <1513010522@qq.com>
2022-12-03 17:22:50 +0800

    update

---
+++ 1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.append(1, [2, 3, 4, 5])
+b = 1 - a
+
+print(a)
+print(b)
---
+++ train_cifar10.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIBLE_DEVICES=1
+MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --learn_sigma True --dropout 0.3"
+DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule cosine"
+TRAIN_FLAGS="--lr 1e-4 --batch_size 128"
+
+python scripts/image_train.py --data_dir /mnt/sda/maxin/dataset/diffusion_dataset/cifar10/ $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
